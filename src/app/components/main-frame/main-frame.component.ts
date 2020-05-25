9127f031dfc93aa58c147e9431299629e58cf578
Jean-Francois Gagnon <jfgagnon@paradox.com>
2020-05-25 09:38:46 -0400

    Work with server as expected

--- src/app/components/main-frame/main-frame.component.ts
+++ src/app/components/main-frame/main-frame.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatCheckboxChange } from '@angular/material/checkbox/checkbox';
-import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { HttpHeaders, HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
@@ -25,39 +25,45 @@ export class MainFrameComponent implements OnInit {
   configs: EditorConfig[];
 
   constructor(private http: HttpClient,
-              private snackBar: MatSnackBar) { }
+    private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
     // call server later
     this.configs = [];
-    for (let i = 0; i < 3; i++) {
-      let cnf = new EditorConfig();
+    this.loadData().subscribe((result) => {
+      this.configs = result;
+    },
+      (err) => {
+        for (let i = 0; i < 3; i++) {
+          let cnf = new EditorConfig();
 
-      cnf.text = 'Type your message here';
-      cnf.background = "beige";
-      cnf.enabled = false;
-      cnf.color = "black";
-      cnf.verticalAlign = false;
-      cnf.horizontalAlign = false;
-      cnf.padding = 0;
-      cnf.font = 'Helvetica';
-      cnf.fontSize = 12;
-      cnf.bold = false;
-      cnf.italic = false;
-      cnf.underline = false;
-      cnf.height = 50;
-      cnf.borderWidth = 0;
-      cnf.borderStyle = 'none';
-      cnf.borderColor = '';
-      cnf.rounded = false;
-      cnf.shadow = false;
+          cnf.text = 'Type your message here';
+          cnf.background = "beige";
+          cnf.enabled = false;
+          cnf.color = "black";
+          cnf.verticalAlign = false;
+          cnf.horizontalAlign = false;
+          cnf.padding = 0;
+          cnf.font = 'Helvetica';
+          cnf.fontSize = 12;
+          cnf.bold = false;
+          cnf.italic = false;
+          cnf.underline = false;
+          cnf.height = 50;
+          cnf.borderWidth = 0;
+          cnf.borderStyle = 'none';
+          cnf.borderColor = '';
+          cnf.rounded = false;
+          cnf.shadow = false;
 
-      this.configs.push(cnf);
-    }
+          this.configs.push(cnf);
+        }
 
-    this.configs[this.INTERNALS].title = "Internals";
-    this.configs[this.CUSTOMERS].title = "Customers";
-    this.configs[this.VISITORS].title = "Visitors";
+        this.configs[this.INTERNALS].title = "Internals";
+        this.configs[this.CUSTOMERS].title = "Customers";
+        this.configs[this.VISITORS].title = "Visitors";
+      });
+
   }
 
   // event handlers
@@ -72,25 +78,29 @@ export class MainFrameComponent implements OnInit {
   }
 
   public saveClicked(e: Event): void {
-    this.saveData().subscribe((result) => {
-      if (result === 'ok') {
-        this.showSnackBar('Save successfully');
-      }
-      else {
-        this.showSnackBar(`Error occured: ${result}`);
-      }
-    },
-    (err) => {
-      this.showSnackBar(`Request failed ${err.status}`);
-    });
+    this.saveData().subscribe(
+      (result) => {
+        if (result === 'ok') {
+          this.showSnackBar('Save successfully');
+        }
+        else {
+          this.showSnackBar(`Error occured: ${result}`);
+        }
+      },
+      (err) => {
+        this.showSnackBar(`Request failed ${err.status}`);
+      });
   }
 
   // helpers functions
 
   public getTabTitle(no: number): string {
-    let t = this.configs[no].title;
-    if (this.configs[no].isDirty) {
-      t += '*';
+    let t = '';
+    if (this.configs.length > 0) {
+      t = this.configs[no].title;
+      if (this.configs[no].isDirty) {
+        t += '*';
+      }
     }
 
     return t;
@@ -99,18 +109,24 @@ export class MainFrameComponent implements OnInit {
   // private code
 
   private saveData(): Observable<string> {
-
-    console.log(JSON.stringify(this.configs, null, 2));
-
     let myHeaders: HttpHeaders = new HttpHeaders();
+    myHeaders.set('Content-Type', 'application/json');
+
     const myBody: any = {
       configs: this.configs
     };
-    myHeaders.set('Content-Type', 'application/json');
 
     return this.http
       .post<string>(`${environment.apiUrl}savemotd`, myBody, { headers: myHeaders })
       .pipe(map((result: string) => result));
+  }
+
+  private loadData(): Observable<EditorConfig[]> {
+    let myHeaders: HttpHeaders = new HttpHeaders();
+    myHeaders.set('Content-Type', 'application/json');
+
+    return this.http.get<EditorConfig[]>(`${environment.apiUrl}loadmotd`, { headers: myHeaders })
+      .pipe(map((data: EditorConfig[]) => data));
   }
 
   private showSnackBar(message: string, action?: string) {
